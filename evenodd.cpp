52fc4d170e6626e9a67dcea440bfab8ffef5fed7
Ramlakhan Lodhi <112104877+ramlakhan79@users.noreply.github.com>
2022-09-30 00:34:16 +0530

    EvenorOdd Commit

---
+++ evenodd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number :";
+    cin>>n;
+    if(n%2==0)
+     cout<<"Given number is Even "<<n;
+    else
+     cout<<"Given number is not Odd  "<<n;
+     return 0;      
+}
