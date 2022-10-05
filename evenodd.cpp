22d65b19567310fd4cf20062404ae2b0ecbae42b
Ramlakhan <ramlakhanlodhi229@gmail.com>
2022-10-05 20:36:38 +0530

    Second Commit

--- evenodd.cpp
+++ evenodd.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 int main(){
@@ -10,17 +9,4 @@ int main(){
     else
      cout<<"Given number is not Odd  "<<n;
      return 0;      
-=======
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cout<<"Enter the number :";
-    cin>>n;
-    if(n%2==0)
-     cout<<"Given number is Even "<<n;
-    else
-     cout<<"Given number is not Odd  "<<n;
-     return 0;      
->>>>>>> 52fc4d170e6626e9a67dcea440bfab8ffef5fed7
 }
--- square.cpp
+++ square.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include"iostream"
 using namespace std;
 int main()
@@ -10,17 +9,4 @@ int main()
   
    cout<<"Area of Square is : "<<a*a;
    return 0;
-=======
-#include"iostream"
-using namespace std;
-int main()
-{
-   int a;
-
-   cout<<"Enter the value of side : ";
-   cin>>a;
-  
-   cout<<"Area of Square is : "<<a*a;
-   return 0;
->>>>>>> fbc1036c79e70241febf209aabc527a7c026a864
 }
