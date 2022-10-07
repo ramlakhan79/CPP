cedbaa8d7cfc0b6b4d5b65cb2667a29fdaafd677
Ramlakhan <ramlakhanlodhi229@gmail.com>
2022-10-07 23:53:11 +0530

    Tringle Commit

---
+++ cpp9.cpp
@@ -0,0 +1,12 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+   int h,w;
+
+   cout<<"Enter the value of Height And Width : ";
+   cin>>h>>w;
+  
+   cout<<"Area of Tringle is : "<<(float)(h*w)/2;
+   return 0;
+}
