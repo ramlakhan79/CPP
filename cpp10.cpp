a175ce9a8920710207afdc5647f06a1b76f28b0f
Ramlakhan <ramlakhanlodhi229@gmail.com>
2022-10-09 00:04:12 +0530

    Cpp10 Commit

---
+++ cpp10.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+ float a,b;
+ float c;
+ cout<<"Enter 2 floats :";
+ cin>>a>>b;
+ c=(int)a/(int)b;
+ cout<<"Reminder is "<<c;
+ return 0;
+}
