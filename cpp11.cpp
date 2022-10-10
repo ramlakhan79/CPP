1e1da02f3a2e4bfc683992cff92cc7b16bf0d0cb
Ramlakhan <ramlakhanlodhi229@gmail.com>
2022-10-10 22:15:24 +0530

    Cpp11 Commit

---
+++ cpp11.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+ int a,b;
+ int  c;
+ cout<<"Enter 2 integer Value :";
+ cin>>a>>b;
+ c=(int)a%(int)b;
+ cout<<"Module is "<<c;
+ return 0;
+}
