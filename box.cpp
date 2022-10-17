b6923f409718ba9ffec237032b62b35d2796c927
Ramlakhan <ramlakhanlodhi229@gmail.com>
2022-10-17 20:48:45 +0530

    Box Commit

---
+++ box.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int l,b,h,v;
+    cout<<"Enter the Box Side L,B & H :: ";
+    cin>>l>>b>>h;
+    v=l*b*h;
+     cout<<"Box volume is  "<<v;   
+     return 0;      
+}
